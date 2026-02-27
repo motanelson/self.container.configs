0043509c22bd7f456e76a16d49af47b0f6f1edab
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-27 00:29:18 +0000

    Add files via upload

--- container.sh
+++ container.sh
@@ -1,5 +1,17 @@
 printf "\033c\033[40;37m\n\n"
+mkdir -p ./{bin,lib,lib64,usr}
+mkdir -p ./lib/x86_64-linux-gnu
+cp /lib/x86_64-linux-gnu/libc.so.6 ./lib
+cp /lib64/ld-linux-x86-64.so.2 ./lib64
+cp /usr/bin/qemu-x86_64 ./bin
+cp /lib/x86_64-linux-gnu/libm.so.6 ./lib/x86_64-linux-gnu
+cp /lib/x86_64-linux-gnu/libglib-2.0.so.0 ./lib/x86_64-linux-gnu/
+cp /lib/x86_64-linux-gnu/libc.so.6 ./lib/x86_64-linux-gnu/
+cp /lib64/ld-linux-x86-64.so.2 ./lib64/
+cp /lib/x86_64-linux-gnu/libpcre2-8.so.0 ./lib/x86_64-linux-gnu/
+cp ./hello ./bin
 gcc -o hello hello.c
+tree ./
 #if you system is other change this
-qemu-x86_64 ./hello
+sudo chroot ./ /bin/qemu-x86_64 /hello
 
